8239af7ef81f31eaa7b3a07c78bda5f421d32fee
冯昊 <fh345392977@gmail.com>
2020-10-20 09:34:10 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  globals: {
+    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
+    page: true,
+    REACT_APP_ENV: true,
+  },
+  plugins: ['react-hooks'],
+  rules: {
+    // ...
+    'react-hooks/rules-of-hooks': 'error', // 检查 Hook 的规则
+    'react-hooks/exhaustive-deps': 'warn', // 检查 effect 的依赖
+  },
+};
