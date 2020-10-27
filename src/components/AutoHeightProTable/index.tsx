06a0e1f26b678178608649f80f73e3525072dbe4
冯昊 <fh345392977@gmail.com>
2020-10-27 23:19:26 +0800

    兼容全部情况的表格高度自适应

--- src/components/AutoHeightProTable/index.tsx
+++ src/components/AutoHeightProTable/index.tsx
@@ -1,29 +1,35 @@
 import { ParamsType } from '@ant-design/pro-provider';
 import ProTable, { ProTableProps } from '@ant-design/pro-table';
 import React, { useEffect, useRef, useState } from 'react';
+import { useFullscreen } from 'ahooks';
+import { useWindowSize } from 'react-use'
+import styles from './style.less';
 
-function AutoHeightProTable<T, U extends ParamsType = {}>(props: ProTableProps<T, U>) {
-  const { formRef, search, showHeader = true, ...rests } = props;
+interface Props<T, U extends ParamsType = {}> extends ProTableProps<T, U> {
 
+}
+
+function AutoHeightProTable<T, U extends ParamsType = {}>(props: Props<T, U>) {
+  const { formRef, search, showHeader = true, pagination, options, ...rests } = props;
   const [collapsed, setCollapsed] = useState(true);
   const wrapperRef = useRef<HTMLDivElement>(null);
   const [scrollY, setScrollY] = useState<number>(0);
-  const [wrapperH, setWrapperH] = useState<string>('100%');
-
-  // 根据页面高度动态设置表格垂直出现滚动的高度
+  const [isFullscreen, { toggleFull }] = useFullscreen(wrapperRef);
+  const {height} = useWindowSize();
   useEffect(() => {
     if (wrapperRef.current) {
       const headerForm = wrapperRef.current.querySelector('.ant-pro-table-search');
       const tableHead = wrapperRef.current.querySelector('.ant-table-thead');
       const tableAction = wrapperRef.current.querySelector('.ant-pro-table-list-toolbar');
-      const wrapperHeight = wrapperRef.current.getBoundingClientRect().height;
-      setWrapperH(`${wrapperHeight}px`);
+      const wrapperHeight = isFullscreen ? height : wrapperRef.current.getBoundingClientRect().height;
       let nextScrollY = wrapperHeight;
       if (tableHead) {
-        nextScrollY -= tableHead.getBoundingClientRect().height;
         if (showHeader) {
-          nextScrollY -= 56;
+          nextScrollY -= tableHead.getBoundingClientRect().height;
         }
+      }
+      if (pagination !== false) {
+        nextScrollY -= 56;
       }
       if (headerForm) {
         const headerFormMargin = 16;
@@ -35,22 +41,28 @@ function AutoHeightProTable<T, U extends ParamsType = {}>(props: ProTableProps<T
       }
       setScrollY(nextScrollY);
     }
-  }, [wrapperRef, formRef, collapsed, showHeader]);
-  console.log('scrollY', scrollY);
+  }, [wrapperRef, formRef, collapsed, showHeader, pagination, isFullscreen, height]);
   return (
-    <div ref={wrapperRef} style={{ height: wrapperH }}>
+    <div ref={wrapperRef} className={styles.wrapper}>
       <ProTable<T, U>
         formRef={formRef}
         showHeader={showHeader}
+        pagination={pagination}
+        options={{
+          fullScreen: toggleFull,
+          setting: true,
+          reload: true,
+          density: true
+        }}
         {...rests}
         search={search ? {
           ...search,
           collapsed,
           onCollapse: (val) => {
+            setCollapsed(val);
             if (search.onCollapse) {
               search.onCollapse(val);
             }
-            setCollapsed(val);
           },
         } : search}
         scroll={{
